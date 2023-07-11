6619fd0b036a42e01f8bf793e21d71e41c0f00bd
Ssejjemba Dan <ssejjembadan@gmail.com>
2023-07-11 23:40:27 +0300

    Updated mongo configurations

--- config/dev.js
+++ config/dev.js
@@ -3,6 +3,6 @@ module.exports = {
     "70265989829-0t7m7ce5crs6scqd3t0t6g7pv83ncaii.apps.googleusercontent.com",
   googleClientSecret: "8mkniDQOqacXtlRD3gA4n2az",
   mongoURI:
-    "mongodb+srv://readonly:0Wc2It8kdXQ8INbh@cluster0.ffbere1.mongodb.net/advnode?retryWrites=true&w=majority",
+    "mongodb+srv://ssejjembadan:r4sa8Mgs8Df2_-a@cluster0.9s68dv8.mongodb.net/?retryWrites=true&w=majority",
   cookieKey: "123123123",
 };
